a1da9461258bf668f132df23e4107103ee719511
Sudharshan <sudhar393@gmail.com>
2018-08-15 22:37:46 +0800

    sleeepy'

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,10 +5,10 @@
             <li><a href="#">HOME</a></li>
             <li><a href="#">HIGHSCORE</a></li>
             <li class="center"><h1>Welcome {{username}}!</h1></li>
-            <li><font-awesome-icon icon="search" /></li>
-            <li><font-awesome-icon icon="heart" /></li>
-            <li><font-awesome-icon icon="bell" /></li>
-            <li><font-awesome-icon icon="search" /></li>
+            <li><font-awesome-icon icon="search" size="lg" /></li>
+            <li><font-awesome-icon icon="heart" size="lg" /></li>
+            <li><font-awesome-icon icon="bell" size="lg" /></li>
+            <li><font-awesome-icon icon="search" size="lg" /></li>
         </ul>
     </nav>
 </template>
@@ -25,6 +25,10 @@
         list-style-type: none;
         padding: 0px;
         margin: 0px;
+    }
+
+    li {
+        padding: 10px;
     }
 
     h1 {
