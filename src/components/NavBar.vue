648120fb1bb5f35bd3927563f10725d204b7eb35
Sudharshan <sudhar393@gmail.com>
2018-08-15 22:22:39 +0800

    some flex styling on navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,14 @@
 <template>
     <nav>
-        <img src="#"/>                          <!-- Website logo -->
-        <ul>                                    <!-- Initial navigation link -->
+        <ul class="navbar" role="navigation">      
+            <li><img src="#"/></li>
             <li><a href="#">HOME</a></li>
             <li><a href="#">HIGHSCORE</a></li>
-        </ul>
-        <h1>Welcome {{username}}!</h1>           <!-- Welcome header -->
-        <ul>    
-            <li><a href="#">Search Icon</a></li>
-            <li><a href="#">Favourites</a></li>
-            <li><a href="#">Notifications</a></li>
-            <li><a href="#">Expand sidetab</a></li>
+            <li class="center"><h1>Welcome {{username}}!</h1></li>
+            <li><font-awesome-icon icon="search" /></li>
+            <li><font-awesome-icon icon="heart" /></li>
+            <li><font-awesome-icon icon="bell" /></li>
+            <li><font-awesome-icon icon="search" /></li>
         </ul>
     </nav>
 </template>
@@ -23,4 +21,20 @@
 </script>
 
 <style scoped>
+    ul {
+        list-style-type: none;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    h1 {
+        margin: 0px;
+    }
+    .navbar {
+        display: flex;
+    }
+    .center {
+        margin-left: auto;
+        margin-right: auto;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faHeart, faBell, faEllipsis } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Global components for fontawesome icons (probably not the best idea)
+library.add(faSearch, faHeart, faBell);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
