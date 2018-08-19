54d51d6f33d5776d33a239dfe2d5d44ddd8886f4
Sudharshan <sudhar393@gmail.com>
2018-08-19 16:54:07 +0800

    fully setup environment

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,22 @@
-console.log('hi')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import config from './config.json'
+
+let app = express()
+
+app.use(morgan('dev'))
+
+app.use(cors({
+  exposedHeaders: config.corsHeaders
+}))
+
+app.use(bodyParser.json({
+  limit: config.bodyLimit
+}))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT || config.port, () => {
+  console.log('Server is active and listening...')
+})
