2b4bdc0b32b58926ce6b02873a4e2886d868ee03
Sudharshan <sudhar393@gmail.com>
2018-08-20 11:01:48 +0800

    added SongItem model

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,17 @@ import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import config from './config.json'
+import mongoose from 'mongoose'
 
+// Setting up MongoDB & mongoose
+mongoose.connect(config.mongooseAddress)
+mongoose.Promise = global.Promise
+
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// Setting up Express.js
 let app = express()
 
 app.use(morgan('dev'))
--- server/src/models/SongItem.js
+++ server/src/models/SongItem.js
@@ -1,1 +1,9 @@
+import {Schema} from 'mongoose'
+
+let SongItemSchema = new Schema({
+    a_string: String,
+    a_date: Date
+})
+
+export default SongItem = mongoose.model('SongItem', SongItemSchema)
 
